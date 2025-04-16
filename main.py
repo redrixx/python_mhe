a142fdcb3f11f2815a171bb31489e708d3797bbb
Jamier Singleton <38224993+redrixx@users.noreply.github.com>
2025-04-16 17:29:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from typing import Optional
+from typing import Union
+from typing import List
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-from fastapi import FastAPI
+from message_parser import *
+
+class DivertMessage(BaseModel):
+    OlpnId: str
+    DivertLocation: str
+
+class DivertList(BaseModel):
+    Data: List[DivertMessage]
+
+class Message(BaseModel):
+    message: str
 
 app = FastAPI()
 
+@app.get("/helloworld")
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/mhe/createDivertMessages")
+def read_divert_list(divertMessages: DivertList):
+    response = {}
+    for divertMessage in divertMessages.Data:
+        response[f"{divertMessage.OlpnId}"] = "Diverted to " + divertMessage.DivertLocation
+    return response
+
+@app.get("/mhe/sendMessage")
+def respond(message: Message):
+    try:
+        return parse_message(message.message)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
